d2c82e5fdd3d10c0179b1f4a034f3c76f3d3ab6d
KarielM <mosley.kariel@gmail.com>
2024-03-19 19:42:32 -0500

    fetched ids

--- src/components/getQuizId.jsx
+++ src/components/getQuizId.jsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 //Started making a component that would get the ID to only pull a single quiz
 export default function QuizID({handleButtonClick}){
+    const [data, setData] = useState([]);
+    let idList = [];
+
+    useEffect(() => {
+        fetch('quizzes.json')
+        .then(response => response.json())
+        .then(data => setData(data))
+    }, [])
+    for(let i = 0; i < data.length; i++){
+        console.log(data[i].quiz.id);
+        idList.push(data[i].quiz.id)
+    }
+    // console.log(data[0].quiz.hasOwnProperty('id'))
+    console.log(idList);
     return (
         <div>
             <button onClick={() => handleButtonClick('python')}>Python</button>
             <button onClick={() => handleButtonClick('html_css')}>HTML/CSS</button>
-            <button onClick={() => handleButtonClick('javascript')}>JavaScript</button>
         </div>
     )
 }
