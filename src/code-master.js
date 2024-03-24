1fed71254566474dcdc2ec959638b47e8c56eb48
rramola <ramolaryan@gmail.com>
2024-03-24 03:03:41 -0500

    Cleaned up workspace

--- src/code-master.js
+++ src/code-master.js
@@ -9,11 +9,10 @@ import DeleteQuiz from "./components/DeleteQuiz/deleteQuiz";
 function CodeMaster() {
   const [quizId, setQuizId] = useState(null);
   const [showJsonHandler, setShowJson] = useState(false);
-  const [modifierId, setModifierId] = useState(null);
   const [numofQuestions, setnumofQuestions] = useState();
   const [name, setName] = useState('')
 
-  function handleButtonClick(id, modifier){
+  function handleButtonClick(id){
     setQuizId(id);
     setShowJson(true);
   }
@@ -25,18 +24,27 @@ function CodeMaster() {
 
   return (
     <div className="App">
-      <DetermineNumberofQuestions prop = {setQuestionsNum}/>
-      <CreateNewQuiz prop ={[numofQuestions, name]}/>
       <div className="header">
         <p>Hello CoderMaster!</p>
       </div>
       <div>
+        {/* VIEW */}
         <QuizID handleButtonClick={handleButtonClick}/>
       </div>
       {showJsonHandler &&
         <div className="appContainer">
+
+          {/* VIEW */}
           <JsonHandler quizId = {quizId}/>
-           <EditQuiz quizId={quizId}/> 
+
+          {/* CREATE */}
+          <DetermineNumberofQuestions prop = {setQuestionsNum}/>
+          <CreateNewQuiz prop ={[numofQuestions, name]}/>
+
+          {/* EDIT */}
+          <EditQuiz quizId={quizId}/> 
+
+          {/* DELETE */}
           <DeleteQuiz quizId={quizId}/>
       </div>
       }
--- src/components/JsonHandling/getQuizId.jsx
+++ src/components/JsonHandling/getQuizId.jsx
@@ -1,15 +1,13 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
-import EditQuiz from '../Updater/updateQuiz';
-//Started making a component that would get the ID to only pull a single quiz
+
 export default function QuizID({handleButtonClick}){
     const [data, setData] = useState([]);
 
     function handleSubmit(e){
         const selectedQuiz = e.target.querySelector('#categorySelector').value;
         const modifierOption = e.target.querySelector('#questionSelector').value;
-        console.log(modifierOption)
-        handleButtonClick(selectedQuiz, modifierOption);
+        handleButtonClick(selectedQuiz);
         document.querySelector('.appContainer').classList.add('hidden')
         e.preventDefault();
     }
@@ -28,18 +26,18 @@ export default function QuizID({handleButtonClick}){
 
     return (
         <div className="appContainer">
-                <form className="selectorForm" onSubmit = {handleSubmit}>
-                    <select id="categorySelector" className="quizDropdown">
-                        {data.map(item => <option key={item} value={item.id}>{item.language}</option>)}
-                    </select>
-                    <select id="questionSelector" className="quizDropdown">
-                        <option value="1">View Quizzes</option>
-                        <option value="2">Create Quiz</option>
-                        <option value="3">Modify Quiz</option>
-                        <option value="4">Delete Quiz</option>
-                    </select>
-                    <button>Submit</button>
-                </form>               
+            <form className="selectorForm" onSubmit = {handleSubmit}>
+                <select id="categorySelector" className="quizDropdown">
+                    {data.map(item => <option key={item} value={item.id}>{item.language}</option>)}
+                </select>
+                <select id="questionSelector" className="quizDropdown">
+                    <option value="1">View Quizzes</option>
+                    <option value="2">Create Quiz</option>
+                    <option value="3">Modify Quiz</option>
+                    <option value="4">Delete Quiz</option>
+                </select>
+                <button>Submit</button>
+            </form>               
         </div>
     )
 }
