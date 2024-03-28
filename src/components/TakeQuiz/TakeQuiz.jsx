2bfa2600700f62fedec69d8e081fa31f23afe804
KarielM <mosley.kariel@gmail.com>
2024-03-28 11:08:38 -0500

    pull/push

--- src/components/TakeQuiz/TakeQuiz.jsx
+++ src/components/TakeQuiz/TakeQuiz.jsx
@@ -1,12 +1,26 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate, useLocation } from 'react-router';
 
 const TakeQuiz = ({ handleGoBack }) => {
   const quizData = useLocation();
+  const [questions, setQuestions] = useState([]);
+  const [answers, setAnswers] = useState({});
   const [wasSubmitted, setWasSubmitted] = useState(false);
   const [finalGrade, setFinalGrade] = useState(0);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    if (quizData.state.questions) {
+      const allQuestions = Object.entries(quizData.state.questions);
+      const selectedQuestions = [];
+      while (selectedQuestions.length < 7) {
+        const randomIndex = Math.floor(Math.random() * allQuestions.length);
+        selectedQuestions.push(allQuestions[randomIndex]);
+        allQuestions.splice(randomIndex, 1);
+      }
+      setQuestions(selectedQuestions);
+    }
+  }, [quizData.state.questions]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -14,15 +28,18 @@ const TakeQuiz = ({ handleGoBack }) => {
   };
 
   const gradeQuiz = (e) => {
-    const numberOfQuestions = e.target.elements.length - 1;
     let numberCorrect = 0;
-    for (let i = 0; i < numberOfQuestions; i++) {
-      if (e.target.elements[i].value.toLowerCase() === e.target.elements[i].dataset.answer.toLowerCase()) {
+    for (let i = 0; i < questions.length; i++) {
+      if (answers[i] && answers[i].toLowerCase() === questions[i][1].toLowerCase()) {
         numberCorrect++;
       }
     }
-    setFinalGrade(Math.round((100 / numberOfQuestions) * numberCorrect));
+    setFinalGrade(Math.round((100 / questions.length) * numberCorrect));
     setWasSubmitted(true);
+  };
+
+  const handleAnswerChange = (e, index) => {
+    setAnswers({ ...answers, [index]: e.target.value });
   };
 
   const handleGoHome = () => {
@@ -34,10 +51,10 @@ const TakeQuiz = ({ handleGoBack }) => {
       {!wasSubmitted ? (
         <form onSubmit={handleSubmit}>
           <ul>
-            {Object.keys(quizData.state.questions).map((question) => (
-              <li key={question}>
-                <p>{question}</p>
-                <input type="text" name="" id="" data-answer={quizData.state.questions[question]} />
+            {questions.map((question, index) => (
+              <li key={index}>
+                <p>{question[0]}</p>
+                <input type="text" name="" id="" value={answers[index] || ''} onChange={(e) => handleAnswerChange(e, index)} required/>
               </li>
             ))}
           </ul>
