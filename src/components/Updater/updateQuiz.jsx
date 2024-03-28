e818cf9b8579446cef98babda64da3a9b53713cc
KarielM <mosley.kariel@gmail.com>
2024-03-27 22:04:40 -0500

    small

--- src/components/Updater/updateQuiz.jsx
+++ src/components/Updater/updateQuiz.jsx
@@ -2,45 +2,55 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
-export default function EditQuiz({ quizId }) {
-  const [singleQuiz, setSingleQuiz] = useState({});
-  const navigate = useNavigate();
 
-  useEffect(() => {
-    const axiosGetOneItem = async (id) => {
-      const response = await axios.get(`http://localhost:9000/getOne/${id}`);
-      setSingleQuiz(response.data);
-    };
-    axiosGetOneItem(quizId);
-  }, [quizId]);
+export default function EditQuiz({quizId, showSplash}){
+    const [singleQuiz, setSingleQuiz] = useState({});
+    const navigate = useNavigate();
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    const newQuestion = e.target.querySelector('#createQuestion').value;
-    const newAnswer = e.target.querySelector('#createAnswer').value;
-
-    if (!isNaN(newQuestion.trim())) {
-    } else {
-      const updatedQuestions = { ...singleQuiz.questions, [newQuestion]: newAnswer };
-      await axios.put(`http://localhost:9000/updateOne`, {
-        id: singleQuiz.id,
-        language: singleQuiz.language,
-        questions: updatedQuestions
+    const axiosGetOneItem = async(id) => {
+      await axios.get(`http://localhost:9000/getOne/${id}`)
+      .then((result) => {
+      setSingleQuiz(result.data)
       });
-      navigate('/QuizID');
     }
-  }
+
+    let questions = singleQuiz.questions;
+
+    const axiosUpdateItem = async(updateObject) => {
+        await axios.put(`http://localhost:9000/updateOne`, updateObject)
+      };
+    
+    function handleSubmiter(e){
+      e.preventDefault();
+      const newQuestion = e.target.querySelector('#createQuestion').value;
+      const newAnswer =  e.target.querySelector('#createAnswer').value;
+      if (!isNaN(newQuestion.trim())) {
+      }
+      else {
+        questions[newQuestion] = newAnswer;
+        axiosUpdateItem( {
+          "id": (singleQuiz.id),
+          "language": (singleQuiz.language),
+          "questions": (questions)
+          })
+        navigate('/QuizID')
+      };
+      }
+
+
+    useEffect(() => {
+      axiosGetOneItem([quizId]);
+    }, [quizId]);
+
 
   return (
     <div className="updateContainer">
       <form className="selectorForm" onSubmit={handleSubmiter}>
-          <h1>Question Creator</h1>
-          <div className='updateFormInputContainer'>
-            <input placeholder='Please type new question here' id="createQuestion" required>
-            </input>
-            <input placeholder='Please type answer here' id="createAnswer" required>
-            </input>
-          </div>
+          <h1>Question creator</h1>
+          <textarea placeholder='Please type new question here' id="createQuestion" required>
+          </textarea>
+          <textarea placeholder='Please type answer here' id="createAnswer" required>
+          </textarea>
           <button>Submit</button>
       </form>               
     </div> 
